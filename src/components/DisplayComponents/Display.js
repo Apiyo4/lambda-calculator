80ddbe76faed8af588e3ee2d592dd6f03b10060c
Apiyo4 <odingoannette@gmail.com>
2019-11-06 10:58:59 +0300

    Refactor code to remove imports notused.

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,9 +1,4 @@
 import React from "react";
-import Numbers from '../ButtonComponents/NumberButtons/NumberButton';
-import Operators from '../ButtonComponents/OperatorButtons/OperatorButton';
-import Specials from '../ButtonComponents/SpecialButtons/SpecialButton';
-
-
 
 const Display = (props) => {
   console.log(props.selectedButton);
